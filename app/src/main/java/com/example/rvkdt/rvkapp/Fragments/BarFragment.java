5268b01247ec8c060cd0803d6eb1bbbfd36c7aef
steinar12 <ssa12@hi.is>
2017-03-01 12:52:19 +0000

    Setti GoogleMap, BarTitle og Rating í mainActivityView

--- app/src/main/java/com/example/rvkdt/rvkapp/Fragments/BarFragment.java
+++ app/src/main/java/com/example/rvkdt/rvkapp/Fragments/BarFragment.java
@@ -1,19 +1,35 @@
 package com.example.rvkdt.rvkapp.Fragments;
 
 
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.widget.TextView;
 
 import com.example.rvkdt.rvkapp.R;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class BarFragment extends Fragment {
 
+    MapView mapView;
+    GoogleMap googleMap;
 
     public BarFragment() {
         // Required empty public constructor
@@ -24,7 +40,67 @@ public class BarFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_bar, container, false);
-    }
+        View view = inflater.inflate(R.layout.fragment_bar, container, false);
 
+        // Creates the map
+        mapView = (MapView) view.findViewById(R.id.map);
+        mapView.onCreate(savedInstanceState);
+        if (mapView != null) {
+            mapView.getMapAsync(new OnMapReadyCallback() {
+                @Override
+                public void onMapReady(GoogleMap googleMap) {
+                    configureMap(googleMap);
+                }
+            });
+        }
+
+        TextView barTitle = (TextView) getActivity().findViewById(R.id.barTitle);
+        barTitle.setText("Austur");
+
+        return view;
+    }
+    private void configureMap(GoogleMap googleMap) {
+        googleMap.addMarker(new MarkerOptions()
+                .icon(BitmapDescriptorFactory.fromResource(R.drawable.common_full_open_on_phone))
+                .anchor(0.0f, 1.0f)
+                .position(new LatLng(55.854049, 13.661331)));
+        googleMap.getUiSettings().setMyLocationButtonEnabled(false);
+        if (ActivityCompat.checkSelfPermission(getActivity(),
+                android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(getActivity(),
+                android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        googleMap.setMyLocationEnabled(true);
+        googleMap.getUiSettings().setZoomControlsEnabled(true);
+        MapsInitializer.initialize(this.getActivity());
+        LatLngBounds.Builder builder = new LatLngBounds.Builder();
+        builder.include(new LatLng(55.854049, 13.661331));
+        LatLngBounds bounds = builder.build();
+        int padding = 0;
+        // Updates the location and zoom of the MapView
+        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngBounds(bounds, padding);
+        googleMap.moveCamera(cameraUpdate);
+        this.googleMap = googleMap;
+    }
+    @Override
+    public void onResume() {
+        mapView.onResume();
+        super.onResume();
+    }
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
 }
